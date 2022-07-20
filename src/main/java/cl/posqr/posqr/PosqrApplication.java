0146200c5b205711ab1de2a9e310658b76628999
Juvenal Salas <juvenal519@gmail.com>
2022-07-20 16:28:27 -0400

    Primer commit DEV

---
+++ src/main/java/cl/posqr/posqr/PosqrApplication.java
@@ -0,0 +1,13 @@
+package cl.posqr.posqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosqrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosqrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/posqr/posqr/PosqrApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.posqr.posqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosqrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
