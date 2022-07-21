184e9efaff5060b664f335fb4ab1db3e437bc162
Juvenal Salas <juvenal519@gmail.com>
2022-07-20 21:22:44 -0400

    Add code_2

--- src/main/java/cl/posqr/posqr/mock/CodeMock.java
+++ src/main/java/cl/posqr/posqr/mock/CodeMock.java
@@ -11,7 +11,9 @@ public class CodeMock {
     public List<Code> getAllCode(){
         List<Code> listCode = new ArrayList<>();
         Code code = new Code("código_1", "activo");
+        Code code_2 = new Code("código_1", "activo");
         listCode.add(code);
+        listCode.add(code_2);
         return listCode;
     }
 
